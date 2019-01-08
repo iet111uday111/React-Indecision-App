dad18fd89ee36cb21298b4d76c28c6dc4d47b7b7
Udayaditya Singh <geekworks2017@gmail.com>
2019-01-08 12:15:07 +0530

    "ES6-Arrow Function-1"

--- src/playground/es6-arrow.js
+++ src/playground/es6-arrow.js
@@ -1,25 +1,20 @@
-'use strict';
-
-var square = function square(x) {
-  return x * x;
+const square = function (x) {
+  return x*x;  
 };
 
 // const squareArrow =  (x) => {
 //     return x*x;  
 // };
 
-var squareArrow = function squareArrow(x) {
-  return x * x;
-};
+const squareArrow =  (x) => x*x;
 
 // const getFirstName = (fullName) => {
 //     return fullName.split(' ')[0]
 // }
 
-var getFirstName = function getFirstName(fullName) {
-  return fullName.split(' ')[0];
-};
+const getFirstName = (fullName) => fullName.split(' ')[0];
 
 console.log(square(8));
 console.log(squareArrow(9));
 console.log(getFirstName('Udayaditya Singh'));
+
